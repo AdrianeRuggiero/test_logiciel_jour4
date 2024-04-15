e079e16cde57ffa8243fa94c2f76046bb49dac51
AdrianeRuggiero <driketes@gmail.com>
2024-04-15 12:46:07 +0200

    flake8

--- Scraping.py
+++ Scraping.py
@@ -2,9 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url ="https://www.bbc.com/portuguese"
+url = "https://www.bbc.com/portuguese"
 
-headers = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+headers = {
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+}
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
